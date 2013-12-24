9c0284fb3948434e9670dbdfb0f498bcd8d79f41
ScottSavarie <scottsavarie@gmail.com>
2013-12-24 13:42:51 -0500

    map range on nav and adding optimizing mobile layout

--- source/js/main.js
+++ source/js/main.js
@@ -9,12 +9,8 @@ if(!Modernizr.svg) {
 }
 
 
-
-
-/* =Parallax Shit
+/* =Map Range Function
 ============================================================================== */
-
-
 
 
 map_range = function(value, low1, high1, low2, high2) {
@@ -25,13 +21,111 @@ map_range = function(value, low1, high1, low2, high2) {
 
 
 
- // var prefix = Modernizr.prefixed('transform');
+
+
+/* =Header resize
+============================================================================== */
+
+var windowHeight = window.innerHeight
+var windowWidth = window.innerWidth
+
+resizeHeader = function(){
+  if (windowWidth >= windowHeight*1.5){
+    $("#header-wrap").height(windowHeight);
+    }
+  else {
+
+    }  
+}
+
+$(document).ready(function() {
+  resizeHeader();
+}); 
+
+
+$(window).resize(function() {
+    $("#header-wrap").removeAttr('style');
+});
+
+
+
+/* =Nav / Logo show
+============================================================================== */
+
+$(document).ready(function() {
+  $(".logo-wrap").addClass("logo-wrap-slide");
+}); 
+
+$(document).ready(function() {
+  $(".nav-wrap").addClass("nav-wrap-show");
+}); 
+
+
+
+
 
 $(window).scroll(function(event) {
   var scrollTop = $(document).scrollTop();
 
-  if (window.innerWidth >= 870 && scrollTop <= 1000){
-    $("#header-img").css({"-webkit-transform": "translate3d" + "(0, " + map_range(scrollTop, 1, 1000, 1, 400) + "px" + ", 0" + ")"});
+  if (window.innerWidth >= 767){
+
+    $(".humco-type").css({opacity : map_range(scrollTop, 1, 500, 0, 1)});
+    $(".nav-links").css({opacity : map_range(scrollTop, 1, 500, 0, 1)});
+  
+
+    }
+
+    else {
+
+    // $(".humco-type").css({opacity : 1});
+    // $(".nav-links").css({opacity : 1});      
+      
+    }
+});
+
+
+// $(window).resize(function() {
+    
+//     if (window.innerWidth <= 766){
+
+//     $(".humco-type").css({opacity : 1});
+//     $(".nav-links").css({opacity : 1});   
+
+//   }
+  
+// });
+
+
+
+
+
+
+
+
+
+
+/* =Parallax Shit
+============================================================================== */
+
+// var prefix = Modernizr.prefixed("transform");
+
+
+
+
+
+
+var header = $("#header-img").offset().top;
+
+$(window).scroll(function(event) {
+  var scrollTop = $(document).scrollTop();
+
+  if (window.innerWidth >= 769 && scrollTop <= header + 1000){
+
+    $("#header-img").css({transform : "translate3d" + "(0, " + map_range(scrollTop, header, header + 1000, 0, 400) + "px" + ", 0" + ")"});
+    $("#header-img").css({MozTransform : "translate3d" + "(0, " + map_range(scrollTop, header, header + 1000, 0, 400) + "px" + ", 0" + ")"});
+    $("#header-img").css({WebkitTransform : "translate3d" + "(0, " + map_range(scrollTop, header, header + 1000, 0, 400) + "px" + ", 0" + ")"});
+    $("#header-img").css({msTransform : "translate3d" + "(0, " + map_range(scrollTop, header, header + 1000, 0, 400) + "px" + ", 0" + ")"});
+    $("#header-img").css({OTransform : "translate3d" + "(0, " + map_range(scrollTop, header, header + 1000, 0, 400) + "px" + ", 0" + ")"});
 
     }
 
@@ -42,45 +136,98 @@ $(window).scroll(function(event) {
 
 
 
-// var models = $("#models-img").offset().top;
+var models = $("#models-img").offset().top;
 
-// $(window).scroll(function(event) {
-//   var scrollTop = $(document).scrollTop();
+$(window).scroll(function(event) {
+  var scrollTop = $(document).scrollTop();
 
-//   if (window.innerWidth >= 870 && scrollTop <= models + 1000){
-//     $("#models-img").css(prefix , "translate3d(0,"+(scrollTop/3)+"px,0)");
+  if (window.innerWidth >= 769 && scrollTop <= models + 1000){
 
-//     }
+    $("#models-img").css({transform : "translate3d" + "(0, " + map_range(scrollTop, models, models + 1000, 0, 400) + "px" + ", 0" + ")"});
+    $("#models-img").css({Moztransform : "translate3d" + "(0, " + map_range(scrollTop, models, models + 1000, 0, 400) + "px" + ", 0" + ")"});
+    $("#models-img").css({Webkittransform : "translate3d" + "(0, " + map_range(scrollTop, models, models + 1000, 0, 400) + "px" + ", 0" + ")"});
+    $("#models-img").css({mstransform : "translate3d" + "(0, " + map_range(scrollTop, models, models + 1000, 0, 400) + "px" + ", 0" + ")"});
+    $("#models-img").css({Otransform : "translate3d" + "(0, " + map_range(scrollTop, models, models + 1000, 0, 400) + "px" + ", 0" + ")"});
 
-//     else {
+
+    }
+
+    else {
       
-//     }
-// });
+    }
+});
 
 
 
+var pti = $("#pti-img").offset().top;
+
+$(window).scroll(function(event) {
+  var scrollTop = $(document).scrollTop();
+
+  if (window.innerWidth >= 769 && scrollTop <= pti + 1000){
+
+    $("#pti-img").css({transform : "translate3d" + "(0, " + map_range(scrollTop, pti, pti + 1000, 0, 400) + "px" + ", 0" + ")"});
+    $("#pti-img").css({Moztransform : "translate3d" + "(0, " + map_range(scrollTop, pti, pti + 1000, 0, 400) + "px" + ", 0" + ")"});
+    $("#pti-img").css({Webkittransform : "translate3d" + "(0, " + map_range(scrollTop, pti, pti + 1000, 0, 400) + "px" + ", 0" + ")"});
+    $("#pti-img").css({mstransform : "translate3d" + "(0, " + map_range(scrollTop, pti, pti + 1000, 0, 400) + "px" + ", 0" + ")"});
+    $("#pti-img").css({Otransform : "translate3d" + "(0, " + map_range(scrollTop, pti, pti + 1000, 0, 400) + "px" + ", 0" + ")"});
 
 
+    }
 
-
-
-
-
-
-// var pti = $("#pti-img").offset().top;
-
-// $(window).scroll(function(event) {
-//   var scrollTop = $(document).scrollTop();
-
-//   if (window.innerWidth >= 870 && scrollTop <= pti + 1000){
-//     $("#pti-img").css({"top": map_range(scrollTop, pti, pti + 1000, 1, 400) + "px"});
-
-//     }
-
-//     else {
+    else {
       
-//     }
-// });
+    }
+});
+
+
+
+
+
+var lil = $("#lil-img").offset().top;
+
+$(window).scroll(function(event) {
+  var scrollTop = $(document).scrollTop();
+
+  if (window.innerWidth >= 769 && scrollTop <= lil + 1000){
+
+    $("#lil-img").css({transform : "translate3d" + "(0, " + map_range(scrollTop, lil, lil + 1000, 0, 400) + "px" + ", 0" + ")"});
+    $("#lil-img").css({Moztransform : "translate3d" + "(0, " + map_range(scrollTop, lil, lil + 1000, 0, 400) + "px" + ", 0" + ")"});
+    $("#lil-img").css({Webkittransform : "translate3d" + "(0, " + map_range(scrollTop, lil, lil + 1000, 0, 400) + "px" + ", 0" + ")"});
+    $("#lil-img").css({mstransform : "translate3d" + "(0, " + map_range(scrollTop, lil, lil + 1000, 0, 400) + "px" + ", 0" + ")"});
+    $("#lil-img").css({Otransform : "translate3d" + "(0, " + map_range(scrollTop, lil, lil + 1000, 0, 400) + "px" + ", 0" + ")"});
+
+
+    }
+
+    else {
+      
+    }
+});
+
+
+
+var old = $("#old-img").offset().top;
+
+$(window).scroll(function(event) {
+  var scrollTop = $(document).scrollTop();
+
+  if (window.innerWidth >= 769 && scrollTop <= old + 1000){
+
+    $("#old-img").css({transform : "translate3d" + "(0, " + map_range(scrollTop, old, old + 1000, 0, 400) + "px" + ", 0" + ")"});
+    $("#old-img").css({Moztransform : "translate3d" + "(0, " + map_range(scrollTop, old, old + 1000, 0, 400) + "px" + ", 0" + ")"});
+    $("#old-img").css({Webkittransform : "translate3d" + "(0, " + map_range(scrollTop, old, old + 1000, 0, 400) + "px" + ", 0" + ")"});
+    $("#old-img").css({mstransform : "translate3d" + "(0, " + map_range(scrollTop, old, old + 1000, 0, 400) + "px" + ", 0" + ")"});
+    $("#old-img").css({Otransform : "translate3d" + "(0, " + map_range(scrollTop, old, old + 1000, 0, 400) + "px" + ", 0" + ")"});
+
+
+    }
+
+    else {
+      
+    }
+});
+
 
 
 
